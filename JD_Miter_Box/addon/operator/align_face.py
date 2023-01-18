86a680f64a4998330e1d76c8335ece39d5ca6a26
JohnKazucki <83173898+JohnKazucki@users.noreply.github.com>
2023-01-18 22:39:00 +0100

    #7 feat: Align Face: slide mode picks best sliding edge
    instead of picking the first edge it finds, it now tries to find one that is closest to the ideal slide direction (if there was no slide edge on the face)

--- JD_Miter_Box/addon/operator/align_face.py
+++ JD_Miter_Box/addon/operator/align_face.py
@@ -268,28 +268,35 @@ class MB_OT_ALIGN_FACE(Operator):
             # get the connected vertices that are not part of the selection
             connected_verts = get_connected_verts(vert, exclude_selected=True)
             connected_faces = get_connected_faces_of_vert(vert, exclude_selected=True)
-            
-            # means there are slide edges we can use
-            if connected_verts:
-                # get the first one
-                vert_end = connected_verts[0]
-                # get vector to it
-                dir = vert_end.co - vert.co
-                dir = dir.normalized()
+
             # means we have adjacent faces to build a slide direction from
-            elif connected_faces:
-                
+            if connected_faces:
                 face_N = connected_faces[0].normal
                 face_tan = face_N.cross(normal)
                 face_bitan = face_tan.cross(face_N)
 
-                dir = face_bitan
-
+                dir_ideal = face_bitan
+                dir = dir_ideal
             # geo has no unselected slide edges or faces, so must be an internal part of the selection
             else:
-                dir = normal
+                dir_ideal = normal
+                dir = dir_ideal
+            
+            # means there are slide edges we can use
+            if connected_verts:
 
-            # append it to dirs list
+                # find the edge to slide on that is most in line with the ideal direction
+                max_dot = 0
+                for vert_end in connected_verts:
+                    dir_option = vert_end.co - vert.co
+                    dir_option = dir_option.normalized()
+
+                    dir_dot = abs(dir_option.dot(dir_ideal))
+                    if dir_dot > max_dot:
+                        max_dot = dir_dot
+                        dir = dir_option
+
+            # append the slide direction to dirs list
             slide_directions.append(dir)
 
         # return list
